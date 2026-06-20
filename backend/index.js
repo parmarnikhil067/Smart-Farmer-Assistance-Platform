cf1c8ba8ea217a8ac9f26330b29b32186819d1a1
parmarnikhil067 <nikhil810parmar@gmail.com>
2026-06-20 16:19:26 +0530

    Updated backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 // const io = new Server(server, { cors: { origin: "*" } });
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000"
+        origin: "https://smart-farmer-assistance-platform-wk.vercel.app/"
     }
 });
 
