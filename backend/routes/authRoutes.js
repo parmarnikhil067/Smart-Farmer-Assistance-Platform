3ebe8773860110898b2f576fde4ecf07dc71fb6f
parmarnikhil067 <nikhil810parmar@gmail.com>
2026-06-21 21:40:23 +0530

    Updated backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,8 +2,8 @@ const router = require("express").Router;
 
 const aRouter = router();
 const authCtrl = require("../controllers/authController");
-aRouter.post("api/register", authCtrl.register);
-aRouter.post("api/login", authCtrl.login);
+aRouter.post("/register", authCtrl.register);
+aRouter.post("/login", authCtrl.login);
 
 
 module.exports = aRouter;
